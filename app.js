7b47e2586961dc9e76acc29c8c9677205e9184fd
Jessica Mansell <jcmansell88@gmail.com>
2021-06-28 14:03:47 -0700

    cleaner way to display the game over prompt

--- app.js
+++ app.js
@@ -205,8 +205,9 @@ document.addEventListener('DOMContentLoaded', () => {
     if (squares[pacmanCurrentIndex].classList.contains('ghost') && !squares[pacmanCurrentIndex].classList.contains('scared-ghost')) {
       ghosts.forEach(ghost => clearInterval(ghost.timerID));
       document.removeEventListener('keyup', movePacman);
-      setTimeout(function(){alert('Game Over!');
-      }, 500);
+      //setTimeout(function(){alert('Game Over!');
+      //}, 500);
+      scoreDisplay.innerHTML = ' GAME OVER';
     }
   }
 
