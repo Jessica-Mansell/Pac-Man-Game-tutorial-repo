bb2020be36f5357768bc35092117e29846127e4d
Jessica Mansell <jcmansell88@gmail.com>
2021-06-28 11:50:21 -0700

    inserted ghosts

--- app.js
+++ app.js
@@ -128,6 +128,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+  ]
 
 
 
